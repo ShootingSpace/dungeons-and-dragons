7271189c994b7165bb03eaec5b4349e36447b741
Cong <erdongerzong@qq.com>
2018-06-22 16:13:34 +0100

    modify TETile

--- byog/TileEngine/Tileset.java
+++ byog/TileEngine/Tileset.java
@@ -18,12 +18,13 @@ import java.awt.Color;
 
 public class Tileset {
     public static final TETile PLAYER = new TETile('@', Color.white, Color.black, "player");
+    public static final TETile FOOTPRINT = new TETile('*', Color.red, Color.black, "player foot print");
     public static final TETile WALL = new TETile('#', new Color(216, 128, 128), Color.darkGray,
             "wall");
     public static final TETile FLOOR = new TETile('·', new Color(128, 192, 128), Color.black,
             "floor");
-    public static final TETile HALLWAY = new TETile('-', Color.orange, Color.black,
-            "break through of room");
+    public static final TETile HALLWAY = new TETile('·', new Color(128, 192, 128), Color.black,
+            "Floor");
     public static final TETile NOTHING = new TETile(' ', Color.black, Color.black, "nothing");
     public static final TETile GRASS = new TETile('"', Color.green, Color.black, "grass");
     public static final TETile WATER = new TETile('≈', Color.blue, Color.black, "water");
