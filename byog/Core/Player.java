64ab6319411cea19f158e7cda827bae9aa5780a5
Cong <erdongerzong@qq.com>
2018-06-22 17:05:08 +0100

    fix player move bug

--- byog/Core/Player.java
+++ byog/Core/Player.java
@@ -13,22 +13,22 @@ class Player implements java.io.Serializable {
 
     void move(TETile[][] world, char cmd){
         if (cmd == 'W'){
-            if (world[p.x][p.y + 1] != Tileset.WALL){
+            if (p.y < world[0].length - 1 && !world[p.x][p.y + 1].equals(Tileset.WALL) ){
                 world[p.x][p.y] = Tileset.FOOTPRINT;
                 p.y += 1;
             }
         } else if (cmd == 'S') {
-            if (world[p.x][p.y - 1] != Tileset.WALL) {
+            if (p.y > 0 && !world[p.x][p.y - 1].equals(Tileset.WALL)) {
                 world[p.x][p.y] = Tileset.FOOTPRINT;
                 p.y -= 1;
             }
         } else if (cmd == 'A'){
-            if (world[p.x - 1][p.y] != Tileset.WALL) {
+            if (p.x > 0 && !world[p.x - 1][p.y].equals(Tileset.WALL)) {
                 world[p.x][p.y] = Tileset.FOOTPRINT;
                 p.x -= 1;
             }
         } else if (cmd == 'D'){
-            if (world[p.x + 1][p.y] != Tileset.WALL) {
+            if (p.x < world.length - 1 && !world[p.x + 1][p.y].equals(Tileset.WALL)) {
                 world[p.x][p.y] = Tileset.FOOTPRINT;
                 p.x += 1;
             }
